9496889dd0056437fad79fa53d262964b0c6114c
Lucas S. Corte <corte1994.lc@gmail.com>
2023-10-01 13:07:05 -0300

    completa a aula de variaveis

---
+++ c#/aulas/Vars/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+// See https://aka.ms/new-console-template for more information
+int idade = 29;
+string nome = "Lucas";
+bool ativo = true;
+decimal salario = 1245.98M;
+float descontos = 398.99f;
+
+string eAtivo = ativo == true ? "ativo" : "inativo";
+
+WriteLine($"O Usuário {nome} tem {idade} anos de idade, está {eAtivo} no sistema. Recebe por mês o salário de R$ {salario}.");
+WriteLine($"Desconto: R$ {descontos}");
+dynamic x = "Esta variável é uma string.";
+WriteLine(x);
+
+x = 10;
+WriteLine(x);
+
+ReadKey();
