ca801b32508429816c3d39d2f5bdac6d9c240ca6
Lucas S. Corte <corte1994.lc@gmail.com>
2023-10-01 00:23:46 -0300

    feat: usando imports statics

--- c#/aulas/HelloWorld/Program.cs
+++ c#/aulas/HelloWorld/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+// See https://aka.ms/new-console-template for more information
+WriteLine("Hello, World!");
